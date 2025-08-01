b3512a6e9c1653fc8bd6ee35dcd53535a31f5b76
Siyam Ahmed <siyam.eng.du@gmail.com>
2025-08-01 22:49:05 +0600

    Completed the Nextjs App Router Tutorial

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,4 +1,6 @@
-import clsx from 'clsx';
+import clsx from "clsx";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { deleteInvoice } from "@/app/lib/actions";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -9,11 +11,26 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className,
+        "flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        className
       )}
     >
       {children}
     </button>
+  );
+}
+
+// ...
+
+export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
+  return (
+    <form action={deleteInvoiceWithId}>
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-4" />
+      </button>
+    </form>
   );
 }
